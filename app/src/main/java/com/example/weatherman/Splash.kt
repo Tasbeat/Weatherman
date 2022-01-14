04b3a0ba457973e95aa4beeb8c46d833adb97d5d
Ali Alirahimi <alialirahimi642@gmail.com>
2022-01-14 17:37:05 +0330

    new methods and edit UI. fixing problems

--- app/src/main/java/com/example/weatherman/Splash.kt
+++ app/src/main/java/com/example/weatherman/Splash.kt
@@ -1,15 +1,14 @@
+package com.example.weatherman
+
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
-import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.weatherman.MainActivity
-import com.example.weatherman.R
 
-internal class Splash : AppCompatActivity() {
+class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash)
